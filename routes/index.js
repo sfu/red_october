f824265833f52df53caf41871cc0e8dd85858651
Graham Ballantyne <grahamb@sfu.ca>
2015-03-13 16:06:52 -0700

    handle errors

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,19 @@ var events = require('events');
 var Promise = require('bluebird');
 var config = require('config');
 
+var errobjconfig = {
+  configurable: true,
+  value: function() {
+    var alt = {};
+    var storeKey = function(key) {
+      alt[key] = this[key];
+    };
+    Object.getOwnPropertyNames(this).forEach(storeKey, this);
+    return alt;
+  }
+};
+Object.defineProperty(Error.prototype, 'toJSON', errobjconfig);
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Red October', session_id: req.sessionID });
 });
@@ -12,13 +25,25 @@ router.get('/', function(req, res, next) {
 
 router.post('/ping', function(req, res, next) {
   var url = req.body.url;
+  var timeout = req.body.timeout || null;
   var publishers = config.get('publishers');
-
   var pings = publishers.map(function(p) {
-    return ping(p + url);
+    var options = timeout ? { timeout: parseInt(timeout) } : {};
+    if (p.indexOf('45034') > -1) {
+      options = {timeout: 5};
+    }
+    return ping(p + url, options).then(function(response) { return response; }).catch(function(error) { return error; });
   });
-  Promise.all(pings).then(function(responses) {JSON
-    res.send(JSON.stringify(responses));
+  Promise.all(pings).then(function(responses) {
+    var ret = { successes: [], failures: []};
+    responses.forEach(function(response) {
+      if (response instanceof Error) {
+        ret.failures.push(response.message);
+      } else {
+        ret.successes.push(response);
+      }
+    });
+    res.send(JSON.stringify(ret));
   })
 });
 
