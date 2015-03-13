b1c5a626ce85715f27726f6459def2a82dea8496
Graham Ballantyne <grahamb@sfu.ca>
2015-03-13 14:57:45 -0700

    implement pinger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var ping = require('../lib/ping');
+var events = require('events');
+var Promise = require('bluebird');
+var config = require('config');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Red October', session_id: req.sessionID });
+});
+
+
+router.post('/ping', function(req, res, next) {
+  var url = req.body.url;
+  var publishers = config.get('publishers');
+
+  var pings = publishers.map(function(p) {
+    return ping(p + url);
+  });
+  Promise.all(pings).then(function(responses) {JSON
+    res.send(JSON.stringify(responses));
+  })
 });
 
 module.exports = router;
