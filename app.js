0296a4fcac917e115317e785fc0c1dd7ea5cdbe8
Graham Ballantyne <grahamb@sfu.ca>
2015-03-23 11:29:48 -0700

    make it work with proxied paths

--- app.js
+++ app.js
@@ -36,9 +36,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
+app.use('/itservices/redoctober', express.static(__dirname + '/public'));
+app.use('/itservices/redoctober', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
